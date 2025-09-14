faa1f7c3c402f9fb23d97803d17df156c7a61fc8
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2025-09-14 15:42:18 -0700

    chore: remove bunfig

--- test/test-setup.ts
+++
@@ -1,5 +0,0 @@
-// Test setup file for bun test
-// This file can be used to set up global test configuration if needed
-
-// Set up any global test utilities or mocks here
-console.log("Test setup loaded");
